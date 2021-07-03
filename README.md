9337e16846305e1c545c104336153673bece2a6d
litencatt <ncl0709@gmail.com>
2021-07-03 16:13:10 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## インストール方法
+1. `$ git clone https://github.com/litencatt/paste-string-filter.git`
+1. `chrome://extensions` を開く
+1. ディベロッパーモードをONにする
+1. `パッケージ化されていない拡張機能を読み込み`を押してcloneしたディレクトリを選ぶ
---
+++ script.js
@@ -0,0 +1,18 @@
+window.addEventListener('paste', (event) => {
+    let clipboardData = event.clipboardData || window.clipboardData || event.originalEvent.clipboardData;
+    let paste = clipboardData.getData('text');
+
+    const mailRegExp = /[\w\-._]+@[\w\-._]+\.[A-Za-z]+/
+    paste = paste.replace(mailRegExp, "(replaced)");
+
+    const elem = window.document.activeElement;
+    if (!elem || !["TEXTAREA"].includes(elem.nodeName)) return false;
+
+    // 選択範囲をpaste文字列で置換
+    orignal = elem.value;
+    selectionStart = elem.selectionStart;
+    selectionEnd = elem.selectionEnd;
+    elem.value = orignal.slice(0, selectionStart) + paste + orignal.slice(selectionEnd);
+
+    event.preventDefault();
+});
