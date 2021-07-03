f887019f14fcf69501eba7842824ded35aa9927c
litencatt <ncl0709@gmail.com>
2021-07-04 01:17:17 +0900

    const

--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ window.addEventListener('paste', (event) => {
     if (!elem || !["TEXTAREA"].includes(elem.nodeName)) return false;
 
     // 選択範囲をpaste文字列で置換
-    orignal = elem.value;
-    selectionStart = elem.selectionStart;
-    selectionEnd = elem.selectionEnd;
+    const orignal = elem.value;
+    const selectionStart = elem.selectionStart;
+    const selectionEnd = elem.selectionEnd;
     elem.value = orignal.slice(0, selectionStart) + paste + orignal.slice(selectionEnd);
 
     event.preventDefault();
