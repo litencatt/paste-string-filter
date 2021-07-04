be367519abcebae476130854f5f00cc60692cc33
litencatt <ncl0709@gmail.com>
2021-07-04 15:23:29 +0900

    Move out

--- src/index.js
+++ src/index.js
@@ -18,7 +18,8 @@ document.addEventListener('paste', (event) => {
   let clipboardData = event.clipboardData || window.clipboardData || event.originalEvent.clipboardData;
   let paste = clipboardData.getData('text');
 
-  const mailRegExp = /[\w\-._]+@[\w\-._]+\.[A-Za-z]+/
+  const mailRegExp = /[\w\-._]+@[\w\-._]+\.[A-Za-z]+/;
+  const replacedStr = "(replaced)";
 
   storage.get(["enable"]).then((items) => {
     if (!items.hasOwnProperty("enable")) {
@@ -32,7 +33,7 @@ document.addEventListener('paste', (event) => {
       return;
     }
 
-    paste = paste.replace(mailRegExp, "(replaced)");
+    paste = paste.replace(mailRegExp, replacedStr);
     elem.value = orignal.slice(0, selectionStart) + paste + orignal.slice(selectionEnd);
   });
 
