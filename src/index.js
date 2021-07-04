38ad13d8b1a3f38c4b147fea93e102016e060e42
litencatt <ncl0709@gmail.com>
2021-07-04 15:07:52 +0900

    Load enable settings in paste callback func.

--- src/index.js
+++ src/index.js
@@ -5,32 +5,42 @@ chrome.storage.local.get(function(result) {
   console.log(result);
 })
 
-let enable = false;
-storage.get(["enable"]).then((items) => {
-  enable = items['enable'];
-  console.log(enable);
-});
+//let enable = false;
+//storage.get(["enable"]).then((items) => {
+//  enable = items['enable'];
+//  console.log(enable);
+//});
 
-window.addEventListener('paste', (event) => {
+document.addEventListener('paste', (event) => {
+  const elem = window.document.activeElement;
+  if (!elem || !["TEXTAREA"].includes(elem.nodeName)) {
+    return false;
+  }
+
+  const orignal = elem.value;
+  const selectionStart = elem.selectionStart;
+  const selectionEnd = elem.selectionEnd;
+
+  let clipboardData = event.clipboardData || window.clipboardData || event.originalEvent.clipboardData;
+  let paste = clipboardData.getData('text');
+
+  const mailRegExp = /[\w\-._]+@[\w\-._]+\.[A-Za-z]+/
+
+  storage.get(["enable"]).then((items) => {
+    if (!items.hasOwnProperty("enable")) {
+      console.log("enable is not set.")
+      return;
+    }
+
+    const enable = items['enable'];
     if (!enable) {
       console.log("Filter is disable now.")
       return;
     }
 
-    let clipboardData = event.clipboardData || window.clipboardData || event.originalEvent.clipboardData;
-    let paste = clipboardData.getData('text');
-
-    const mailRegExp = /[\w\-._]+@[\w\-._]+\.[A-Za-z]+/
     paste = paste.replace(mailRegExp, "(replaced)");
-
-    const elem = window.document.activeElement;
-    if (!elem || !["TEXTAREA"].includes(elem.nodeName)) return false;
-
-    // 選択範囲をpaste文字列で置換
-    const orignal = elem.value;
-    const selectionStart = elem.selectionStart;
-    const selectionEnd = elem.selectionEnd;
     elem.value = orignal.slice(0, selectionStart) + paste + orignal.slice(selectionEnd);
+  });
 
-    event.preventDefault();
+  event.preventDefault();
 });
