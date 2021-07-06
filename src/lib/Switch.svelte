04597008e56271c7e0f5c58511c574b0132dff04
dojineko <dojineko.spiral@gmail.com>
2021-07-07 01:53:47 +0900

    rebuild frontend with svelte

---
+++ src/lib/Switch.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import { onMount } from 'svelte'
+  import { storage } from '../storage'
+  import Grid from './Grid.svelte'
+
+  const getStatus = async (): Promise<boolean> => {
+    // @ts-ignore
+    return (await storage.get(['enable'])).enable
+  }
+
+  const setStatus = async (enable: boolean) => {
+    storage.set({ enable })
+  }
+
+  const onChange: svelte.JSX.FormEventHandler<HTMLInputElement> = async (e) => {
+    const checked = e.currentTarget.checked
+    await setStatus(checked)
+    status = await getStatus()
+  }
+
+  let status: boolean
+  onMount(async () => {
+    status = await getStatus()
+  })
+</script>
+
+<Grid>
+  <span slot="title">Feature</span>
+  <label>
+    <input checked={status} on:change={onChange} type="checkbox" name="status" value="enabled" /> Enabled
+  </label>
+</Grid>
