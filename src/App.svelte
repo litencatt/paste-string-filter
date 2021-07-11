8138444e05b942cb6b9b3eab90efbe58fda39c66
litencatt <ncl0709@gmail.com>
2021-07-11 13:40:40 +0900

    Expand view

--- src/App.svelte
+++ src/App.svelte
@@ -18,7 +18,7 @@
 
   :global(body) {
     color: #455a64;
-    width: 200px;
+    width: 300px;
     padding: 10px;
     font-size: 14px;
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif,
