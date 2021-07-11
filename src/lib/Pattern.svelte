d0475655b34f8a5cc21f00406ce8e1804f375e0e
litencatt <ncl0709@gmail.com>
2021-07-11 13:44:36 +0900

    Show saved regexps

--- src/lib/Pattern.svelte
+++ src/lib/Pattern.svelte
@@ -1,20 +1,48 @@
 <script lang="ts">
+  import { onMount } from 'svelte'
   import Grid from './Grid.svelte'
+  import { storage } from '../storage'
+  //import { storage } from '../localStorage'
+  import type { Regexp } from '../interface'
+
+  const getRegexps = async (): Promise<boolean> => {
+    // @ts-ignore
+    return await storage.get(['regexps'])
+  }
+
+  let tmp: any
+  let regexps: Regexp[] = []
+  onMount(async () => {
+    tmp = await getRegexps()
+    regexps = tmp.regexps
+  })
 </script>
 
 <Grid>
-  <span slot="title">Pattern</span>
-  <div class="inputWrap"><input class="input" /></div>
-  <div><button class="save" disabled>Save</button></div>
+  <span slot="title">Patterns</span>
+  {#each regexps as regexp}
+    <ul>
+      <div class="inputWrap">
+        <input class="input" id="name" bind:value={regexp.name} />
+        <input class="input" id="regexp" bind:value={regexp.regexp} />
+        <div />
+      </div>
+    </ul>
+  {/each}
+  <div><button class="save">Save</button></div>
 </Grid>
 
 <style>
   .inputWrap {
     margin-bottom: 0.25rem;
+    display: inline-flex;
   }
   .input {
     display: block;
-    width: 100%;
+    width: 70%;
+  }
+  .input#name {
+    width: 30%;
   }
   .save {
     display: block;
