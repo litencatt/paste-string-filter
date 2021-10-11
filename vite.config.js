bb2ebb4839fcbfc4ebda4d45cf71fc3fdad4264f
tascript <wataruscript8611@gmail.com>
2021-10-12 01:20:42 +0900

    use material design

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import sveltePreprocess from 'svelte-preprocess'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,5 +20,7 @@ export default defineConfig({
       },
     },
   },
-  plugins: [svelte()],
+  plugins: [svelte({
+      preprocess: sveltePreprocess()
+    })],
 })
